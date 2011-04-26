d7ea83adad0c3822e8279dd0c138082f0e44c380
Matt Rubin <mattrubin@me.com>
2011-04-25 21:39:55 -0700

    New Rails app without the Test::Unit framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '336d8c45597024ee85ee6e0265f8d849088df3a8491fc98e411be79bb9e6be19a5b5a73eafc107316d92e0a64ab9be7df84b0ed2389063aec322452ea0b4b2ff'
