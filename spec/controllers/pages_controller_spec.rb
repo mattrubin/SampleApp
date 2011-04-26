5704004b273fafecd945df7ac8ff900c758c7358
Matt Rubin <mattrubin@me.com>
2011-04-26 00:50:12 -0700

    Completed 3.5 Exercises #1

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -38,4 +38,17 @@ describe PagesController do
       response.should have_selector("title", :content => " | About")
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => " | Help")
+    end
+  end
+  
 end
